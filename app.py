0016c3c2bf7169778ff85145167a47fe36916d79
egoppel <egoppel@gmail.com>
2016-07-06 12:19:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return "Hello World, This is my python app deployed with shipped.io"
 
 
 if __name__ == "__main__":
